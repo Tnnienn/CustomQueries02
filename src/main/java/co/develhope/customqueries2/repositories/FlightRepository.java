835503361738987b5ae70d49bc827402549860c7
Antonio Fasanella <fasanella.antonio.sanfele@gmail.com>
2022-12-21 14:36:24 +0100

    Initial commit

---
+++ src/main/java/co/develhope/customqueries2/repositories/FlightRepository.java
@@ -0,0 +1,15 @@
+package co.develhope.customqueries2.repositories;
+
+import co.develhope.customqueries2.entities.Flight;
+import co.develhope.customqueries2.entities.FlightStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+    Page<Flight> findAllByStatus(FlightStatus status, PageRequest of);
+
+    List<Flight> getCustomFlight(FlightStatus p1, FlightStatus p2);
+}
